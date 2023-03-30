2a1c527e9419e697b2e29332ca8a67c2aacf671f
Anne L'Hôte <anne.lhote@gmail.com>
2023-03-30 13:29:41 +0200

    feat(countries): Add all countries from OpenAlex

---
+++ client/src/components/Loader/index.jsx
@@ -0,0 +1,34 @@
+import './style.scss';
+
+import { Col, Container, Row } from '@dataesr/react-dsfr';
+import PropTypes from 'prop-types';
+
+export default function LoadingSpinner({ size, spacing }) {
+  return (
+    <Container>
+      <Row alignItems="middle" spacing={spacing} justifyContent="center">
+        <Col>
+          <svg
+            className="loader"
+            style={{ height: size, width: size }}
+            viewBox="0 0 120 120"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle className="internal-circle" cx="60" cy="60" r="30" />
+            <circle className="external-circle" cx="60" cy="60" r="50" />
+          </svg>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+LoadingSpinner.propTypes = {
+  size: PropTypes.string,
+  spacing: PropTypes.string,
+};
+
+LoadingSpinner.defaultProps = {
+  size: '100px',
+  spacing: 'py-15w px-auto',
+};
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,23 +1,28 @@
-import { Container, Title, Select } from '@dataesr/react-dsfr';
+/* eslint-disable no-await-in-loop */
+import { Container, SearchableSelect, Title } from '@dataesr/react-dsfr';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import { useEffect, useState } from 'react';
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+import Loader from '../components/Loader';
+
+const api = 'https://api.openalex.org/works';
+const mailto = 'bso@recherche.gouv.fr';
+
+// eslint-disable-next-line no-promise-executor-return
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const getCountries = async () => {
+  const response = await fetch(`${api}?group_by=institutions.country_code`);
+  const data = await response.json();
+  const countries = data.group_by.filter((item) => item.key !== 'unknown').map((item) => ({ value: item.key, label: item.key_display_name, count: item.count }));
+  return countries;
+};
 
 export default function Home() {
-  const api = 'https://api.openalex.org/works';
-  const countries = [
-    { value: 'fr', label: 'France' },
-    { value: 'dk', label: 'Denmark' },
-    { value: 'de', label: 'Germany' },
-    { value: 'pt', label: 'Portugal' },
-  ];
-  const mailto = 'bso@recherche.gouv.fr';
-
-  const [country, setCountry] = useState(countries[0].value);
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const [options, setOptions] = useState({
     chart: {
       type: 'column',
@@ -36,41 +41,50 @@ export default function Home() {
   });
 
   useEffect(() => {
+    async function getData() {
+      const data = await getCountries();
+      setCountries(data.map((item) => ({ value: item.value, label: `${item.label} (${item.count.toLocaleString()})` })));
+      setCountry(data[0].value);
+    }
+    getData();
+  }, []);
+
+  useEffect(() => {
     const getData = async () => {
-      const oaRepository = [];
-      const oaPublisher = [];
-      const oaRepositoryPublisher = [];
-      const years = ['2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021'];
-      // eslint-disable-next-line no-restricted-syntax, guard-for-in
-      for (const year of years) {
-        // eslint-disable-next-line no-await-in-loop
-        const response1 = await fetch(`${api}?filter=institutions.country_code:${country},publication_year:${year}&group_by=best_oa_location.is_oa&mailto=${mailto}`);
-        // eslint-disable-next-line no-await-in-loop
-        const data1 = await response1.json();
-        const oaTotal = data1.group_by.find((item) => item.key === 'true').count;
-        // eslint-disable-next-line no-await-in-loop
-        const response2 = await fetch(`${api}?filter=institutions.country_code:${country},publication_year:${year},best_oa_location.is_oa:true&group_by=locations.source.type&mailto=${mailto}`);
-        // eslint-disable-next-line no-await-in-loop
-        const data2 = await response2.json();
-        const y = data2.group_by.find((item) => item.key === 'repository').count;
-        const z = data2.group_by.find((item) => item.key === 'journal').count;
-        oaRepository.push(oaTotal - z);
-        oaPublisher.push(oaTotal - y);
-        oaRepositoryPublisher.push(z + y - oaTotal);
-        // eslint-disable-next-line no-await-in-loop
-        await sleep(100);
+      if (country && country !== '') {
+        await getCountries();
+        setIsLoading(true);
+        const oaRepository = [];
+        const oaPublisher = [];
+        const oaRepositoryPublisher = [];
+        const years = ['2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021'];
+        // eslint-disable-next-line no-restricted-syntax
+        for (const year of years) {
+          const response1 = await fetch(`${api}?filter=institutions.country_code:${country},publication_year:${year}&group_by=best_oa_location.is_oa&mailto=${mailto}`);
+          const data1 = await response1.json();
+          const oaTotal = data1.group_by.find((item) => item.key === 'true').count;
+          const response2 = await fetch(`${api}?filter=institutions.country_code:${country},publication_year:${year},best_oa_location.is_oa:true&group_by=locations.source.type&mailto=${mailto}`);
+          const data2 = await response2.json();
+          const y = data2.group_by.find((item) => item.key === 'repository').count;
+          const z = data2.group_by.find((item) => item.key === 'journal').count;
+          oaRepository.push(oaTotal - z);
+          oaPublisher.push(oaTotal - y);
+          oaRepositoryPublisher.push(z + y - oaTotal);
+          await sleep(1000);
+        }
+        const options2 = JSON.parse(JSON.stringify(options));
+        options2.xAxis = { categories: years };
+        options2.series = [
+          { name: 'Publisher', data: oaPublisher },
+          { name: 'Publisher & open repositories', data: oaRepositoryPublisher },
+          { name: 'Open repositories', data: oaRepository },
+        ];
+        options2.colors = ['#ead737', '#91ae4f', '#19905b'];
+        const countryLabel = countries.find((item) => item.value === country).label;
+        options2.title = { text: `Distribution of the open access rate of publications in ${countryLabel} according to OpenAlex` };
+        setOptions(options2);
+        setIsLoading(false);
       }
-      const options2 = JSON.parse(JSON.stringify(options));
-      options2.xAxis = { categories: years };
-      options2.series = [
-        { name: 'Publisher', data: oaPublisher },
-        { name: 'Publisher & open repositories', data: oaRepositoryPublisher },
-        { name: 'Open repositories', data: oaRepository },
-      ];
-      options2.colors = ['#ead737', '#91ae4f', '#19905b'];
-      const countryLabel = countries.find((item) => item.value === country).label;
-      options2.title = { text: `Distribution of the open access rate of publications in ${countryLabel} according to OpenAlex` };
-      setOptions(options2);
     };
     getData();
   }, [country]);
@@ -80,15 +94,25 @@ export default function Home() {
       <Title as="h1">
         BSOpenAlex
       </Title>
-      <Select
-        onChange={(e) => setCountry(e.target.value)}
+      <SearchableSelect
+        onChange={(e) => setCountry(e)}
         options={countries}
         selected={country}
       />
-      <HighchartsReact
-        highcharts={Highcharts}
-        options={options}
-      />
+      {isLoading && (
+        <div
+          className="graph-container text-center"
+          style={{ height: '400px' }}
+        >
+          <Loader />
+        </div>
+      )}
+      {!isLoading && (
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={options}
+        />
+      )}
     </Container>
   );
 }
