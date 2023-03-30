507e04cc0fa97124a653f999c8b9bd971263c848
Anne L'Hôte <anne.lhote@gmail.com>
2023-03-30 14:28:15 +0200

    feat(graph): Set data as percent

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -7,7 +7,9 @@ import { useEffect, useState } from 'react';
 import Loader from '../components/Loader';
 
 const api = 'https://api.openalex.org/works';
+const defaultCountry = 'fr';
 const mailto = 'bso@recherche.gouv.fr';
+const sleepDuration = 500;
 
 // eslint-disable-next-line no-promise-executor-return
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -15,7 +17,7 @@ const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const getCountries = async () => {
   const response = await fetch(`${api}?group_by=institutions.country_code`);
   const data = await response.json();
-  const countries = data.group_by.filter((item) => item.key !== 'unknown').map((item) => ({ value: item.key, label: item.key_display_name, count: item.count }));
+  const countries = data.group_by.filter((item) => item.key !== 'unknown').map((item) => ({ value: item.key.toLowerCase(), label: item.key_display_name, count: item.count }));
   return countries;
 };
 
@@ -33,9 +35,6 @@ export default function Home() {
     plotOptions: {
       column: {
         stacking: 'normal',
-        dataLabels: {
-          enabled: true,
-        },
       },
     },
   });
@@ -43,8 +42,8 @@ export default function Home() {
   useEffect(() => {
     async function getData() {
       const data = await getCountries();
-      setCountries(data.map((item) => ({ value: item.value, label: `${item.label} (${item.count.toLocaleString()})` })));
-      setCountry(data[0].value);
+      setCountries(data);
+      setCountry(data.find((item) => item.value === defaultCountry).value);
     }
     getData();
   }, []);
@@ -52,37 +51,40 @@ export default function Home() {
   useEffect(() => {
     const getData = async () => {
       if (country && country !== '') {
-        await getCountries();
         setIsLoading(true);
+        const countryLabel = countries.find((item) => item.value === country).label;
         const oaRepository = [];
         const oaPublisher = [];
         const oaRepositoryPublisher = [];
-        const years = ['2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021'];
+        const years = ['2016', '2017', '2018', '2019', '2020', '2021'];
         // eslint-disable-next-line no-restricted-syntax
         for (const year of years) {
           const response1 = await fetch(`${api}?filter=institutions.country_code:${country},publication_year:${year}&group_by=best_oa_location.is_oa&mailto=${mailto}`);
           const data1 = await response1.json();
           const oaTotal = data1.group_by.find((item) => item.key === 'true').count;
+          const total = data1.group_by.find((item) => item.key === 'true').count + data1.group_by.find((item) => item.key === 'unknown').count;
           const response2 = await fetch(`${api}?filter=institutions.country_code:${country},publication_year:${year},best_oa_location.is_oa:true&group_by=locations.source.type&mailto=${mailto}`);
           const data2 = await response2.json();
           const y = data2.group_by.find((item) => item.key === 'repository').count;
           const z = data2.group_by.find((item) => item.key === 'journal').count;
-          oaRepository.push(oaTotal - z);
-          oaPublisher.push(oaTotal - y);
-          oaRepositoryPublisher.push(z + y - oaTotal);
-          await sleep(1000);
+          oaRepository.push((oaTotal - z) / total * 100);
+          oaPublisher.push((oaTotal - y) / total * 100);
+          oaRepositoryPublisher.push((z + y - oaTotal) / total * 100);
+          await sleep(sleepDuration);
         }
-        const options2 = JSON.parse(JSON.stringify(options));
-        options2.xAxis = { categories: years };
-        options2.series = [
+        const optionsCopy = JSON.parse(JSON.stringify(options));
+        optionsCopy.xAxis = { categories: years };
+        optionsCopy.series = [
           { name: 'Publisher', data: oaPublisher },
           { name: 'Publisher & open repositories', data: oaRepositoryPublisher },
           { name: 'Open repositories', data: oaRepository },
         ];
-        options2.colors = ['#ead737', '#91ae4f', '#19905b'];
-        const countryLabel = countries.find((item) => item.value === country).label;
-        options2.title = { text: `Distribution of the open access rate of publications in ${countryLabel} according to OpenAlex` };
-        setOptions(options2);
+        optionsCopy.colors = ['#ead737', '#91ae4f', '#19905b'];
+        optionsCopy.title = { text: `Distribution of the open access rate of publications in ${countryLabel} according to OpenAlex` };
+        optionsCopy.plotOptions.column.dataLabels = { enabled: true, formatter() {
+          return Number(this.y).toFixed(0).concat(' %');
+        }};
+        setOptions(optionsCopy);
         setIsLoading(false);
       }
     };
@@ -96,7 +98,7 @@ export default function Home() {
       </Title>
       <SearchableSelect
         onChange={(e) => setCountry(e)}
-        options={countries}
+        options={countries.map((item) => ({ value: item.value, label: `${item.label} (${item.count.toLocaleString()})` }))}
         selected={country}
       />
       {isLoading && (
