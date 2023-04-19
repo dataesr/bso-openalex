72fa38b13d804485537081d1b23a1cf17bad43e1
Anne L'Hôte <anne.lhote@gmail.com>
2023-04-19 18:14:15 +0200

    fix(graphs): Add error support while fetching data

--- client/src/components/charts/oa-color-distribution.jsx
+++ client/src/components/charts/oa-color-distribution.jsx
@@ -15,93 +15,97 @@ const OAColorDistribution = ({ countryCodes, countryLabels }) => {
 
   useEffect(() => {
     const getData = async () => {
-      if (countryCodes && countryCodes !== '') {
-        setIsLoading(true);
-        const years = ['2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021'];
-        const closed = [];
-        const green = [];
-        const gold = [];
-        const bronze = [];
-        const hybrid = [];
-        const unknown = [];
-        for (const year of years) {
-          const response = await fetch(`${api}?filter=institutions.country_code:${countryCodes},publication_year:${year},has_doi:true&group_by=open_access.oa_status&mailto=${mailto}`);
-          const data = await response.json();
-          const total = data.group_by.reduce((acc, curr) => acc + curr.count, 0);
-          const yAbsClosed = data.group_by.find((item) => item.key === 'closed')?.count || 0;
-          closed.push({
-            y: yAbsClosed / total * 100,
-            y_abs: yAbsClosed,
-            y_tot: total,
-            year,
-          });
-          const yAbsGreen = data.group_by.find((item) => item.key === 'green')?.count || 0;
-          green.push({
-            y: yAbsGreen / total * 100,
-            y_abs: yAbsGreen,
-            y_tot: total,
-            year,
-          });
-          const yAbsGold = data.group_by.find((item) => item.key === 'gold')?.count || 0;
-          gold.push({
-            y: yAbsGold / total * 100,
-            y_abs: yAbsGold,
-            y_tot: total,
-            year,
-          });
-          const yAbsBronze = data.group_by.find((item) => item.key === 'bronze')?.count || 0;
-          bronze.push({
-            y: yAbsBronze / total * 100,
-            y_abs: yAbsBronze,
-            y_tot: total,
-            year,
-          });
-          const yAbsHybrid = data.group_by.find((item) => item.key === 'hybrid')?.count || 0;
-          hybrid.push({
-            y: yAbsHybrid / total * 100,
-            y_abs: yAbsHybrid,
-            y_tot: total,
-            year,
-          });
-          const yAbsUnknown = data.group_by.find((item) => item.key === 'unknown')?.count || 0;
-          unknown.push({
-            y: yAbsUnknown / total * 100,
-            y_abs: yAbsUnknown,
-            y_tot: total,
-            year,
-          });
-          await sleep(sleepDuration);
-        }
-        const optionsCopy = JSON.parse(JSON.stringify(defaultChartOptions));
-        optionsCopy.xAxis.categories = years;
-        optionsCopy.series = [
-          { name: 'Closed', data: closed },
-          { name: 'Green', data: green },
-          { name: 'Gold', data: gold },
-          { name: 'Bronze', data: bronze },
-          { name: 'Hybrid', data: hybrid },
-          { name: 'Unknown', data: unknown },
-        ];
-        optionsCopy.colors = ['black', 'green', 'yellow', 'orange', 'pink', 'grey'];
-        optionsCopy.legend.title = { text: 'Color' };
-        optionsCopy.title = { text: `Distribution of the open access colors of publications with DOI in ${countryLabels} according to OpenAlex` };
-        optionsCopy.plotOptions.column.dataLabels = {
-          enabled: true, formatter() {
-            return Number(this.y).toFixed(0).concat(' %');
+      try {
+        if (countryCodes && countryCodes !== '') {
+          setIsLoading(true);
+          const years = ['2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021'];
+          const closed = [];
+          const green = [];
+          const gold = [];
+          const bronze = [];
+          const hybrid = [];
+          const unknown = [];
+          for (const year of years) {
+            const response = await fetch(`${api}?filter=institutions.country_code:${countryCodes},publication_year:${year},has_doi:true&group_by=open_access.oa_status&mailto=${mailto}`);
+            const data = await response.json();
+            const total = data.group_by.reduce((acc, curr) => acc + curr.count, 0);
+            const yAbsClosed = data.group_by.find((item) => item.key === 'closed')?.count || 0;
+            closed.push({
+              y: yAbsClosed / total * 100,
+              y_abs: yAbsClosed,
+              y_tot: total,
+              year,
+            });
+            const yAbsGreen = data.group_by.find((item) => item.key === 'green')?.count || 0;
+            green.push({
+              y: yAbsGreen / total * 100,
+              y_abs: yAbsGreen,
+              y_tot: total,
+              year,
+            });
+            const yAbsGold = data.group_by.find((item) => item.key === 'gold')?.count || 0;
+            gold.push({
+              y: yAbsGold / total * 100,
+              y_abs: yAbsGold,
+              y_tot: total,
+              year,
+            });
+            const yAbsBronze = data.group_by.find((item) => item.key === 'bronze')?.count || 0;
+            bronze.push({
+              y: yAbsBronze / total * 100,
+              y_abs: yAbsBronze,
+              y_tot: total,
+              year,
+            });
+            const yAbsHybrid = data.group_by.find((item) => item.key === 'hybrid')?.count || 0;
+            hybrid.push({
+              y: yAbsHybrid / total * 100,
+              y_abs: yAbsHybrid,
+              y_tot: total,
+              year,
+            });
+            const yAbsUnknown = data.group_by.find((item) => item.key === 'unknown')?.count || 0;
+            unknown.push({
+              y: yAbsUnknown / total * 100,
+              y_abs: yAbsUnknown,
+              y_tot: total,
+              year,
+            });
+            await sleep(sleepDuration);
           }
+          const optionsCopy = JSON.parse(JSON.stringify(defaultChartOptions));
+          optionsCopy.xAxis.categories = years;
+          optionsCopy.series = [
+            { name: 'Closed', data: closed },
+            { name: 'Green', data: green },
+            { name: 'Gold', data: gold },
+            { name: 'Bronze', data: bronze },
+            { name: 'Hybrid', data: hybrid },
+            { name: 'Unknown', data: unknown },
+          ];
+          optionsCopy.colors = ['black', 'green', 'yellow', 'orange', 'pink', 'grey'];
+          optionsCopy.legend.title = { text: 'Color' };
+          optionsCopy.title = { text: `Distribution of the open access colors of publications with DOI in ${countryLabels} according to OpenAlex` };
+          optionsCopy.plotOptions.column.dataLabels = {
+            enabled: true, formatter() {
+              return Number(this.y).toFixed(0).concat(' %');
+            }
+          };
+          optionsCopy.yAxis.stackLabels = {
+            enabled: true, formatter() {
+              return Number(this.total).toFixed(0).concat(' %');
+            }
+          };
+          optionsCopy.tooltip = {
+            headerFormat: '',
+            pointFormat: '<b>Publication year {point.year}</b><br>• Access rate<br>with hosting {point.series.name}:<br>{point.y:.2f}% ({point.y_abs} / {point.y_tot})'
+          };
+          setOptions(optionsCopy);
+          setIsLoading(false);
         };
-        optionsCopy.yAxis.stackLabels = {
-          enabled: true, formatter() {
-            return Number(this.total).toFixed(0).concat(' %');
-          }
-        };
-        optionsCopy.tooltip = {
-          headerFormat: '',
-          pointFormat: '<b>Publication year {point.year}</b><br>• Access rate<br>with hosting {point.series.name}:<br>{point.y:.2f}% ({point.y_abs} / {point.y_tot})'
-        };
-        setOptions(optionsCopy);
-        setIsLoading(false);
-      };
+      } catch (error) {
+        setIsError(true);
+      }
     };
     getData();
   }, [countryCodes, countryLabels]);
--- client/src/components/charts/oa-status-distribution.jsx
+++ client/src/components/charts/oa-status-distribution.jsx
@@ -14,72 +14,76 @@ const OAStatusDistribution = ({ countryCodes, countryLabels }) => {
 
   useEffect(() => {
     const getData = async () => {
-      if (countryCodes && countryCodes !== '') {
-        setIsLoading(true);
-        const years = ['2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021'];
-        const oaRepository = [];
-        const oaPublisher = [];
-        const oaRepositoryPublisher = [];
-        for (const year of years) {
-          const response1 = await fetch(`${api}?filter=institutions.country_code:${countryCodes},publication_year:${year},has_doi:true&group_by=best_oa_location.is_oa&mailto=${mailto}`);
-          const data1 = await response1.json();
-          const oaTotal = data1.group_by.find((item) => item.key === 'true').count;
-          const total = data1.group_by.find((item) => item.key === 'true').count + data1.group_by.find((item) => item.key === 'unknown').count;
-          const response2 = await fetch(`${api}?filter=institutions.country_code:${countryCodes},publication_year:${year},has_doi:true,best_oa_location.is_oa:true&group_by=locations.source.type&mailto=${VITE_OPENALEX_MAILTO}`);
-          const data2 = await response2.json();
-          const y = data2.group_by.find((item) => item.key === 'repository').count;
-          const z = data2.group_by.find((item) => item.key === 'journal').count;
-          oaRepository.push({
-            y: (oaTotal - z) / total * 100,
-            y_abs: oaTotal - z,
-            y_oa: oaTotal,
-            y_tot: total,
-            year,
-          });
-          oaPublisher.push({
-            y: (oaTotal - y) / total * 100,
-            y_abs: oaTotal - y,
-            y_oa: oaTotal,
-            y_tot: total,
-            year,
-          });
-          oaRepositoryPublisher.push({
-            y: (z + y - oaTotal) / total * 100,
-            y_abs: oaTotal - y,
-            y_oa: oaTotal,
-            y_tot: total,
-            year,
-          });
-          await sleep(sleepDuration);
+      try {
+        if (countryCodes && countryCodes !== '') {
+          setIsLoading(true);
+          const years = ['2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021'];
+          const oaRepository = [];
+          const oaPublisher = [];
+          const oaRepositoryPublisher = [];
+          for (const year of years) {
+            const response1 = await fetch(`${api}?filter=institutions.country_code:${countryCodes},publication_year:${year},has_doi:true&group_by=best_oa_location.is_oa&mailto=${mailto}`);
+            const data1 = await response1.json();
+            const oaTotal = data1.group_by.find((item) => item.key === 'true').count;
+            const total = data1.group_by.find((item) => item.key === 'true').count + data1.group_by.find((item) => item.key === 'unknown').count;
+            const response2 = await fetch(`${api}?filter=institutions.country_code:${countryCodes},publication_year:${year},has_doi:true,best_oa_location.is_oa:true&group_by=locations.source.type&mailto=${VITE_OPENALEX_MAILTO}`);
+            const data2 = await response2.json();
+            const y = data2.group_by.find((item) => item.key === 'repository').count;
+            const z = data2.group_by.find((item) => item.key === 'journal').count;
+            oaRepository.push({
+              y: (oaTotal - z) / total * 100,
+              y_abs: oaTotal - z,
+              y_oa: oaTotal,
+              y_tot: total,
+              year,
+            });
+            oaPublisher.push({
+              y: (oaTotal - y) / total * 100,
+              y_abs: oaTotal - y,
+              y_oa: oaTotal,
+              y_tot: total,
+              year,
+            });
+            oaRepositoryPublisher.push({
+              y: (z + y - oaTotal) / total * 100,
+              y_abs: oaTotal - y,
+              y_oa: oaTotal,
+              y_tot: total,
+              year,
+            });
+            await sleep(sleepDuration);
+          }
+          const optionsCopy = JSON.parse(JSON.stringify(defaultChartOptions));
+          optionsCopy.xAxis.categories = years;
+          optionsCopy.series = [
+            { name: 'Publisher', data: oaPublisher },
+            { name: 'Publisher & open repositories', data: oaRepositoryPublisher },
+            { name: 'Open repositories', data: oaRepository },
+          ];
+          optionsCopy.colors = ['#ead737', '#91ae4f', '#19905b'];
+          optionsCopy.legend.title = { text: 'Hosting type' };
+          optionsCopy.title = { text: `Distribution of the open access status of publications with DOI in ${countryLabels} according to OpenAlex` };
+          optionsCopy.plotOptions.column.dataLabels = {
+            enabled: true,
+            formatter() {
+              return Number(this.y).toFixed(0).concat('%');
+            }
+          };
+          optionsCopy.yAxis.stackLabels = {
+            enabled: true,
+            formatter() {
+              return Number(this.total).toFixed(0).concat('%');
+            }
+          };
+          optionsCopy.tooltip = {
+            headerFormat: '',
+            pointFormat: '<b>Publication year {point.year}</b><br>• Open access rate<br>with hosting {point.series.name}:<br>{point.y:.2f}% ({point.y_abs} / {point.y_tot})<br>• Total open access rate:<br>{point.stackTotal:.2f}% ({point.y_oa} / {point.y_tot})'
+          };
+          setOptions(optionsCopy);
+          setIsLoading(false);
         }
-        const optionsCopy = JSON.parse(JSON.stringify(defaultChartOptions));
-        optionsCopy.xAxis.categories = years;
-        optionsCopy.series = [
-          { name: 'Publisher', data: oaPublisher },
-          { name: 'Publisher & open repositories', data: oaRepositoryPublisher },
-          { name: 'Open repositories', data: oaRepository },
-        ];
-        optionsCopy.colors = ['#ead737', '#91ae4f', '#19905b'];
-        optionsCopy.legend.title = { text: 'Hosting type' };
-        optionsCopy.title = { text: `Distribution of the open access status of publications with DOI in ${countryLabels} according to OpenAlex` };
-        optionsCopy.plotOptions.column.dataLabels = {
-          enabled: true,
-          formatter() {
-            return Number(this.y).toFixed(0).concat('%');
-          }
-        };
-        optionsCopy.yAxis.stackLabels = {
-          enabled: true,
-          formatter() {
-            return Number(this.total).toFixed(0).concat('%');
-          }
-        };
-        optionsCopy.tooltip = {
-          headerFormat: '',
-          pointFormat: '<b>Publication year {point.year}</b><br>• Open access rate<br>with hosting {point.series.name}:<br>{point.y:.2f}% ({point.y_abs} / {point.y_tot})<br>• Total open access rate:<br>{point.stackTotal:.2f}% ({point.y_oa} / {point.y_tot})'
-        };
-        setOptions(optionsCopy);
-        setIsLoading(false);
+      } catch (error) {
+        setIsError(true);
       }
     };
     getData();
